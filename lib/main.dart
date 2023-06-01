feea8e460e60aff7f8ec854ba94f4be1412238c9
Alvaro Sebastian Tabares <astg0616@gmail.com>
2023-05-31 22:39:56 -0500

    GoRouter configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
-import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
-import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-// Named routes have many problems, but can still be used, refer to the official flutter navigation documentation for more information
+// Named routes have many problems, you can use them but got some limitations, refer to the official flutter navigation documentation for more information
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 3).getTheme(),
-      home: const HomeScreen(),
-      routes: {
-        '/buttons': (context) => const ButtonsScreen(),
-        '/cards': (context) => const CardsScreen(),
-      },
+      // home: const HomeScreen(),
+      // routes: {
+      //   '/buttons': (context) => const ButtonsScreen(),
+      //   '/cards': (context) => const CardsScreen(),
+      // },
     );
   }
 }
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -50,7 +51,7 @@ class _CustomListTile extends StatelessWidget {
         color: colors.primary,
       ),
       onTap: () {
-        Navigator.pushNamed(context, menuItem.url);
+        context.push(menuItem.url);
       },
     );
   }
